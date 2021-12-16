4a0fcc37594ccd36e76d33d43c276e58e9b70c50
Noman <noman@noman.land>
2021-12-15 22:25:16 -0500

    Solve part 2

--- js/day1/day1.js
+++ js/day1/day1.js
@@ -1,13 +1,26 @@
 import { get } from '../utils/ajax.js';
 
-get('https://adventofcode.com/2021/day/1/input')
-  .then(({ data }) =>
-    data.split('\n').reduce((total, curr, i, list) => {
-      if (i > 0 && parseInt(curr, 10) > parseInt(list[i - 1], 10)) {
-        return total + 1;
-      }
+const data = await get('https://adventofcode.com/2021/day/1/input').then(
+  ({ data }) => data.split('\n').map(number => parseInt(number, 10))
+);
 
-      return total;
-    }, 0)
-  )
-  .then(console.log);
+const part1 = data.reduce((total, curr, i, list) => {
+  if (i > 0 && curr > list[i - 1]) {
+    return total + 1;
+  }
+
+  return total;
+}, 0);
+
+const part2 = data.reduce((total, curr, i, list) => {
+  const now = curr + list[i - 1] + list[i - 2];
+  const before = list[i - 1] + list[i - 2] + list[i - 3];
+  if (i > 2 && now > before) {
+    return total + 1;
+  }
+
+  return total;
+}, 0);
+
+console.log('part 1: ', part1);
+console.log('part 2: ', part2);
